10e47474e5e786c0b0d8934d4e73b607d69592c2
GROS Christian <christian-gros-pro@outlook.fr>
2022-09-26 23:28:16 +0200

    build 2

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -3,7 +3,7 @@ const Services = () => {
     <section id="services">
       <div className="h-screen mt-40 pl-0 xl:pl-32 flex flex-col justify-center items-center ">
         <h2 className="mb-20 text-2xl ">
-          Voici les services que je peut offrir
+          Voici les services que je peux offrir
         </h2>
         <div className="m-6 grid grid-rows-3 grid-flow-col gap-20 xl:gap-x-52 gap-x-6">
           <div className="flex flex-col  justify-center items-center ">
